032657bc872796c2a15602a91b71b8c84bd999da
John Lee <boblee167@gmail.com>
2019-05-25 14:57:13 +0100

    Add project files.

---
+++ ContractManager4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContractManager4.MvcApplication" Language="C#" %>
---
+++ ContractManager4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContractManager4.Startup))]
+namespace ContractManager4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
